2db366cdc25e683d05659b9ca86e4fff7259b097
Samuel <samuel.chendaxiao@gmail.com>
2023-05-22 18:47:35 +0800

    update：更换底图为天地图矢量

--- src/assets/utils/addWhiteModel.js
+++ src/assets/utils/addWhiteModel.js
@@ -10,7 +10,55 @@ export const addWhiteModel = (viewer,DTGlobe)=>{
         // DTGlobe.whiteModel=tileset;
         //默认不可见
         // tileset.show=false;
-
+        var boundingSphere = tileset.boundingSphere;
+        viewer.camera.viewBoundingSphere(boundingSphere, new Cesium.HeadingPitchRange(0, -2.0, 0));
+        viewer.camera.lookAtTransform(Cesium.Matrix4.IDENTITY);
+    
+        // tileset.style = new Cesium.Cesium3DTileStyle({
+        //     color: {
+        //         conditions: [
+        //             ['true', 'rgba(0, 127.5, 255 ,1)']//'rgb(127, 59, 8)']
+        //         ]
+        //     }
+        // });
+        // console.log(tileset.tileVisible,'tileset.tileVisible')
+        // tileset.tileVisible.addEventListener(function (tile) {
+        //     let content=tile.content
+        //     var featuresLength = content.featuresLength;
+        //     for (var i = 0; i < featuresLength; i += 2) {
+        //         let feature = content.getFeature(i)
+        //         let model = feature.content._model
+        //         // console.log(model,'model')
+        //         if (model && model._sourcePrograms && model._rendererResources) {
+        //             console.log('ok')
+        //             Object.keys(model._sourcePrograms).forEach(key => {
+        //                 let program = model._sourcePrograms[key]
+        //                 let fragmentShader = model._rendererResources.sourceShaders[program.fragmentShader];
+        //                 let v_position = "";
+        //                 if (fragmentShader.indexOf(" v_positionEC;") != -1) {
+        //                     v_position = "v_positionEC";
+        //                 } else if (fragmentShader.indexOf(" v_pos;") != -1) {
+        //                     v_position = "v_pos";
+        //                 }
+        //                 const color = `vec4(${feature.color.toString()})`;
+        //                 model._rendererResources.sourceShaders[program.fragmentShader] =
+        //                     "varying vec3 " + v_position + ";\n" +
+        //                     "void main(void){\n" +
+        //                     "    vec4 position = czm_inverseModelView * vec4(" + v_position + ",1);\n" +
+        //                     "    float glowRange = 360.0;\n" +
+        //                     "    gl_FragColor = "+color+";\n"+
+        //                     "    gl_FragColor *= vec4(vec3(position.z / 100.0), 1.0);\n" +
+        //                     "    float time = fract(czm_frameNumber / 360.0);\n" +
+        //                     "    time = abs(time - 0.5) * 2.0;\n" +
+        //                     "    float diff = step(0.005, abs( clamp(position.z / glowRange, 0.0, 1.0) - time));\n" +
+        //                     "    gl_FragColor.rgb += gl_FragColor.rgb * (1.0 - diff);\n" +
+        //                     "}\n"
+        //             })
+        //             model._shouldRegenerateShaders = true
+        //         }
+        //     }
+        // })
     });
+
     
 }
--- src/components/DTGlobe.vue
+++ src/components/DTGlobe.vue
@@ -19,6 +19,7 @@ import { addFlyLines } from "@/assets/utils/addFlyLines";
 import { addChannal } from "@/assets/utils/addChannel"
 import { addCakeMap } from "@/assets/utils/addCakeMap";
 import { addWhiteModel } from "@/assets/utils/addWhiteModel"
+// import { getProviderViewModels } from "@/assets/utils//provider.js";
 //bus实例用于通信
 import bus from '@/assets/utils/bus'
 export default {
@@ -88,8 +89,10 @@ export default {
             //设置token
             Cesium.Ion.defaultAccessToken =
             'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiI1MmIxYWJmNy0yZDA1LTRiYmQtYmI3Ny1iMGIwNTk5NWQyMWYiLCJpZCI6Mjk5MjQsImlhdCI6MTU5OTIwMDkxOX0.aUw9ehdKoobH0GEq5lp3s3Uk9_QSMZVvFFrsLsAACqc'
+            // let [tiandiVecModel, tiandiImgModel] = getProviderViewModels();
             //viewer初始化
             const viewer = new Cesium.Viewer('cesiumContainer',{
+                // imageryProviderViewModels:[tiandiVecModel, tiandiImgModel] ,
                 geocoder: false,
                 homeButton: false,
                 sceneModePicker: false,
@@ -130,17 +133,34 @@ export default {
                 console.log(viewer.camera.position,viewer.camera.heading,viewer.camera.pitch,viewer.camera.roll)
             }, Cesium.ScreenSpaceEventType.LEFT_CLICK)
             //加载暗色底图
-            let lightEsri = new Cesium.ArcGisMapServerImageryProvider({
-              url: "http://map.geoq.cn/ArcGIS/rest/services/ChinaOnlineStreetPurplishBlue/MapServer",
-              proxy: new Cesium.DefaultProxy('/proxy/')
-            });
-            let lightEsriLayer = viewer.imageryLayers.addImageryProvider(lightEsri);
-            lightEsriLayer.hue=1.6
-            lightEsriLayer.contrast=1.6
-            lightEsriLayer.hue=0
-            lightEsriLayer.saturation=0.66
+            // let lightEsri = new Cesium.ArcGisMapServerImageryProvider({
+            //   url: "http://map.geoq.cn/ArcGIS/rest/services/ChinaOnlineStreetPurplishBlue/MapServer",
+            //   proxy: new Cesium.DefaultProxy('/proxy/')
+            // });
+            // let lightEsriLayer = viewer.imageryLayers.addImageryProvider(lightEsri);
+            // lightEsriLayer.hue=1.6
+            // lightEsriLayer.contrast=1.6
+            // lightEsriLayer.hue=0
+            // lightEsriLayer.saturation=0.66
+            let darklayer=viewer.imageryLayers.addImageryProvider(new Cesium.WebMapTileServiceImageryProvider({  //调用矢量地图中文注记服务
+             url: "http://t{s}.tianditu.gov.cn/vec_w/wmts?service=wmts&request=GetTile&version=1.0.0&LAYER=vec&tileMatrixSet=w&TileMatrix={TileMatrix}&TileRow={TileRow}&TileCol={TileCol}&style=default&format=tiles&tk=61f68aa6b5c7bcbbc0bc95028e8af220",
+             subdomains:['0','1','2','3','4','5','6','7'],
+             layer: "tdtAnnoLayer",
+             style: "default",
+             format: "image/jpeg",
+              tileMatrixSetID: "GoogleMapsCompatible",
+            }));
+            darklayer.hue=3
+            darklayer.contrast=-1.2
+            // let marklayer=viewer.imageryLayers.addImageryProvider(new Cesium.WebMapTileServiceImageryProvider({  //调用矢量地图中文注记服务
+            //  url: "http://t{s}.tianditu.com/cva_w/wmts?service=wmts&request=GetTile&version=1.0.0&LAYER=cva&tileMatrixSet=w&TileMatrix={TileMatrix}&TileRow={TileRow}&TileCol={TileCol}&style=default&format=tiles&tk=61f68aa6b5c7bcbbc0bc95028e8af220",
+            //  subdomains:['0','1','2','3','4','5','6','7'],
+            //  layer: "tdtAnnoLayer",
+            //  style: "default",
+            //  format: "image/jpeg",
+            //   tileMatrixSetID: "GoogleMapsCompatible",
+            // }));
             // 初始化定位
-            // this.resetView()
             this.resetView()
             //添加城市人口柱状图
             this.addCitiesPopulationColumn(cities,viewer,DTGlobe)
