b41c1ccd8045c02c092187cb297bb31020ad6fcc
停云 <1021965604@qq.com>
2023-05-24 10:29:08 +0800

    fix: 地图容器高度铺满

--- src/components/DTGlobe.vue
+++ src/components/DTGlobe.vue
@@ -223,8 +223,8 @@ export default {
   background-size: contain;
 }
 html,body,#cesiumContainer {
-   width:100%;
-  height: 100%;
+  width:100%;
+  height: 100vh;
   margin: 0;
   padding: 0;
   overflow: hidden;
